b0e228a691fd9548fddb9a9b465ac5d37e7022dc
Patri22k <bajzik.patrik6@gmail.com>
2025-06-14 12:47:16 +0200

    refactor: debugging env variables for vercel configuration

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log("ENV variables: \n", import.meta.env.VITE_AUTH_BASE_URL, "\n", import.meta.env.VITE_API_BASE_URL);
 // const API_BASE_URL = 'http://localhost:3000/api'; // Fallback URL for local development
 
 export const fetchTodos = async () => {
